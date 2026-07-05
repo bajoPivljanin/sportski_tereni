193a2b3735a40e459fd30739280e2898b51be00d
losmiafk <milosantunovic1@gmail.com>
2026-07-05 21:10:10 +0200

    Dodavanje fonta

--- inc/header.php
+++ inc/header.php
@@ -6,6 +6,9 @@
     <title>Sports Center</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 </head>
 <body>
 
