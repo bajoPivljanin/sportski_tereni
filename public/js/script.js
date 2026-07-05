7f3897f021dcbe580a60d0db833aa336c0bbf9ca
losmiafk <milosantunovic1@gmail.com>
2026-07-05 23:46:06 +0200

    Ispravka JS greske

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,15 @@
-// Selektujemo elemente sa stranice
+
 const registerTrigger = document.getElementById('register-trigger');
 const registerOverlay = document.getElementById('register-overlay');
 const registerClose = document.getElementById('register-close');
-// Kada korisnik klikne na "Registracija" u navigaciji
+
 registerTrigger.addEventListener('click', function(event) {
-    event.preventDefault(); // Sprečava podrazumevano osvežavanje stranice
-    registerOverlay.classList.add('active'); // Prikazuje zatamnjenje i formu
+    event.preventDefault(); 
+    registerOverlay.classList.add('active');
 });
-// Kada korisnik klikne na dugme X
 registerClose.addEventListener('click', function() {
-    registerOverlay.classList.remove('active'); // Skriva zatamnjenje i formu
+    registerOverlay.classList.remove('active'); 
 });
-// BONUS: Ako korisnik klikne bilo gde u prazno crnilo van forme, modal se zatvara
 registerOverlay.addEventListener('click', function(event) {
     if (event.target === registerOverlay) {
         registerOverlay.classList.remove('active');
