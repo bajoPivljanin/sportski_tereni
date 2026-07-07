a66b5a58340f9c701d41e4c3415c41a3246446fc
losmiafk <milosantunovic1@gmail.com>
2026-07-07 23:26:07 +0200

    Popravka koda

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 require_once 'inc/header.php';
 ?>
 
-    <section class="hero-section d-flex align-items-center text-white position-relative">
-        <div class="hero-overlay position-absolute top-0 start-0 w-100 h-100"></div>
+    <section class="hero-section">
+    <div class="hero-overlay"></div>
 
-        <div class="container position-relative z-1">
-            <div class="row">
-                <div class="col-md-7 col-lg-6 text-start">
-                    <h1 class="hero-title fw-bold mb-4">
-                        Rezerviši teren<br>za par sekundi.
-                    </h1>
-                    <a href="#courts" class="hero-btn">Pogledaj ponudu</a>
-                </div>
+    <div class="container hero-container">
+        <div class="row">
+            <div class="col-md-7 col-lg-6">
+                <h1 class="hero-title">
+                    Rezerviši teren<br>za par sekundi.
+                </h1>
+                <a href="#courts" class="hero-btn">Pogledaj ponudu</a>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
     <main id="courts" class="container my-5">
         <section class="offer-section">
